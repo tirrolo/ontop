59c09bb4f402bf699d9d79457f7094660d383d16
emastranzo <emastranzo@example.org>
2010-07-06 16:19:23 +0000

    Filter that accepts any mapping containing the test in the body

--- erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingSQLStringTreeModelFilter.java
+++ erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingSQLStringTreeModelFilter.java
@@ -3,16 +3,28 @@ package inf.unibz.it.obda.gui.swing.treemodel.filter;
 import inf.unibz.it.obda.domain.OBDAMappingAxiom;
 import inf.unibz.it.obda.rdbmsgav.domain.RDBMSSQLQuery;
 
+/**
+ * @author
+ * This filter receives a string in the constructor and returns true if any mapping contains the string in the body.
+ *
+ */
 public class MappingSQLStringTreeModelFilter implements
 		TreeModelFilter<OBDAMappingAxiom> {
 
 	private String srtSQLStringTreeModelFilter;
 
+	/**
+	 * @param srtSQLStringTreeModelFilter
+	 * Constructor of the filter
+	 */
 	public MappingSQLStringTreeModelFilter(String srtSQLStringTreeModelFilter) {
 		this.srtSQLStringTreeModelFilter=srtSQLStringTreeModelFilter;
 	}
 
-	@SuppressWarnings("deprecation")
+	
+	/* (non-Javadoc)
+	 * @see inf.unibz.it.obda.gui.swing.treemodel.filter.TreeModelFilter#match(java.lang.Object)
+	 */
 	@Override
 	public boolean match(OBDAMappingAxiom object) {
 		// TODO Auto-generated method stub
