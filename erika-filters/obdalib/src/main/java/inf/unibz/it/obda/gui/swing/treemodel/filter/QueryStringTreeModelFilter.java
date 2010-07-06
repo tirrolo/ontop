74e18b9f99407acbb553e2373420bc2d543b0460
emastranzo <emastranzo@example.org>
2010-07-06 16:20:14 +0000

    Filter that accepts any query with that contains the given text.

--- erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/QueryStringTreeModelFilter.java
+++ erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/QueryStringTreeModelFilter.java
@@ -5,15 +5,31 @@ import inf.unibz.it.obda.rdbmsgav.domain.RDBMSSQLQuery;
 import inf.unibz.it.ucq.domain.ConjunctiveQuery;
 import inf.unibz.it.ucq.parser.exception.QueryParseException;
 
+/**
+ * @author This filter receives a string in the constructor and returns true if accepts any query that contains the given text and
+ *         implements the interface TreeModelFilter
+ */
+
 public class QueryStringTreeModelFilter implements
 		TreeModelFilter<OBDAMappingAxiom> {
 
 	private String srtQueryTreeFilter;
 
+	/**
+	 * @param srtQueryTreeFilter
+	 *            Constructor of the Filter
+	 */
 	public QueryStringTreeModelFilter(String srtQueryTreeFilter) {
 		this.srtQueryTreeFilter = srtQueryTreeFilter;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * inf.unibz.it.obda.gui.swing.treemodel.filter.TreeModelFilter#match(java
+	 * .lang.Object)
+	 */
 	@Override
 	public boolean match(OBDAMappingAxiom object) {
 		// TODO Auto-generated method stub
