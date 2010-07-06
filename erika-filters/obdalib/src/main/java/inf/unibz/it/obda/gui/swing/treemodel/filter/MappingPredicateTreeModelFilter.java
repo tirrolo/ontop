884692b4c4b505f19f7c33aac1a4564248c81a95
emastranzo <emastranzo@example.org>
2010-07-06 16:18:59 +0000

    Filter that accepts any mapping containing an atom in the head whose predicate matches predicate.

--- erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingPredicateTreeModelFilter.java
+++ erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingPredicateTreeModelFilter.java
@@ -7,15 +7,31 @@ import inf.unibz.it.ucq.domain.ConjunctiveQuery;
 import inf.unibz.it.ucq.domain.QueryAtom;
 import inf.unibz.it.ucq.domain.QueryTerm;
 
+/**
+ * @author This filter receives a string like parameter in the constructor and returns true if any mapping contains an atom in the head whose
+ *         predicate matches predicate
+ * 
+ */
 public class MappingPredicateTreeModelFilter implements
 		TreeModelFilter<OBDAMappingAxiom> {
 
 	private String srtPredicateFilter;
 
+	/**
+	 * @param srtPredicateFilter
+	 *            Constructor of the filter
+	 */
 	public MappingPredicateTreeModelFilter(String srtPredicateFilter) {
 		this.srtPredicateFilter = srtPredicateFilter;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * inf.unibz.it.obda.gui.swing.treemodel.filter.TreeModelFilter#match(java
+	 * .lang.Object)
+	 */
 	@Override
 	public boolean match(OBDAMappingAxiom object) {
 		boolean filterValue = false;
@@ -26,7 +42,7 @@ public class MappingPredicateTreeModelFilter implements
 		int atomscount = atoms.size();
 		for (int i = 0; i < atomscount; i++) {
 			QueryAtom atom = atoms.get(i);
-			
+
 			if (atom.getName().indexOf(srtPredicateFilter) != -1)
 				filterValue = true;
 
