727c5771079ef705e9474afc6379d6301e8d9df0
emastranzo <emastranzo@example.org>
2010-07-06 16:18:11 +0000

    Filter that accepts any mapping containing the functor in some of the atoms in the head

--- erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingFunctorTreeModelFilter.java
+++ erika-filters/obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingFunctorTreeModelFilter.java
@@ -9,15 +9,32 @@ import inf.unibz.it.ucq.domain.QueryAtom;
 import inf.unibz.it.ucq.domain.QueryTerm;
 import inf.unibz.it.ucq.domain.VariableTerm;
 
+/**
+ * @author This filter receives a string and returns true if any mapping contains the functor in some of the
+ *         atoms in the head
+ * 
+ */
+
 public class MappingFunctorTreeModelFilter implements
 		TreeModelFilter<OBDAMappingAxiom> {
 
 	private String strMappingFunctor = "";
 
+	/**
+	 * @param strMappingFunctor
+	 *            Constructor of the function
+	 */
 	public MappingFunctorTreeModelFilter(String strMappingFunctor) {
 		this.strMappingFunctor = strMappingFunctor;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * inf.unibz.it.obda.gui.swing.treemodel.filter.TreeModelFilter#match(java
+	 * .lang.Object)
+	 */
 	@Override
 	public boolean match(OBDAMappingAxiom object) {
 		boolean filterValue = false;
@@ -36,8 +53,7 @@ public class MappingFunctorTreeModelFilter implements
 				if (term instanceof VariableTerm) {
 					if (term.toString().indexOf(strMappingFunctor) != -1)
 						filterValue = true;
-				}
-				else if (term instanceof FunctionTerm) {
+				} else if (term instanceof FunctionTerm) {
 					FunctionTerm functor = (FunctionTerm) term;
 					ArrayList<QueryTerm> functorTerms = functor.getParameters();
 					for (QueryTerm funcTerm : functorTerms) {
