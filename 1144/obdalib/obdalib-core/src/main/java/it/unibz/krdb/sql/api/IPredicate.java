66f20f553af267897c606d90b500a053a8927782
Mariano Rodriguez-Muro <rodriguez@inf.unibz.it>
2011-10-12 09:20:13 +0000

    Debuging branch rev 1144

---
+++ 1144/obdalib/obdalib-core/src/main/java/it/unibz/krdb/sql/api/IPredicate.java
@@ -0,0 +1,8 @@
+package it.unibz.krdb.sql.api;
+
+/**
+ * Base interface for predicates.
+ */
+public interface IPredicate {
+
+}
---
+++ 1144/obdalib/obdalib-protege4/src/main/java/it/unibz/krdb/obda/protege4/core/ProtegeOBDAOWLReformulationPlatformFactory.java
@@ -0,0 +1,10 @@
+package it.unibz.krdb.obda.protege4.core;
+
+import it.unibz.krdb.obda.owlrefplatform.core.QuestOWLFactory;
+
+import org.protege.editor.owl.model.inference.ProtegeOWLReasonerFactory;
+
+public class ProtegeOBDAOWLReformulationPlatformFactory extends QuestOWLFactory implements
+		ProtegeOWLReasonerFactory {
+
+}
