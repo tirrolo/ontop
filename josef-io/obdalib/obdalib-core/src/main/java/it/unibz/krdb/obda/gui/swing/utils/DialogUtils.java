5f055035154e621a28c96e84f27db6c420f12b5e
Josef Hardi <josef.hardi@gmail.com>
2012-06-28 16:17:20 +0000

    Make the GUI for error report bigger.

--- josef-io/obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/gui/swing/utils/DialogUtils.java
+++ josef-io/obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/gui/swing/utils/DialogUtils.java
@@ -61,10 +61,11 @@ public class DialogUtils {
 		writer.write("#######################\n\n");
 		e.printStackTrace(new PrintWriter(writer));
 		textArea.setText(writer.toString());
+		textArea.setCaretPosition(0);
 
 		// stuff it in a scrollpane with a controlled size.
 		JScrollPane scrollPane = new JScrollPane(textArea);
-		scrollPane.setPreferredSize(new Dimension(640, 150));
+		scrollPane.setPreferredSize(new Dimension(800, 450));
 
 		// pass the scrollpane to the joptionpane.
 		JOptionPane.showMessageDialog(parent, scrollPane, message, JOptionPane.ERROR_MESSAGE);
