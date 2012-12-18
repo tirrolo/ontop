9badc658d44f347fd5066f82461f3d04a1c1bea5
Josef Hardi <josef.hardi@gmail.com>
2012-12-18 13:36:33 +0000

    Fix bug on handling datetime in Oracle.

--- sparql10/obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/JDBCUtility.java
+++ sparql10/obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/JDBCUtility.java
@@ -171,6 +171,16 @@ public class JDBCUtility implements Serializable {
 		}
 		bf.insert(0, "'");
 		bf.append("'");
+		
+		/*
+		 * Oracle has a special treatment for datetime datatype such that it requires a default
+		 * datetime format. In this case, the default is 'YYYY-MM-DD HH24:MI:SS.FF' as in SPARQL
+		 * standard, e.g., to_date('2012-12-18 09:58:23.2','YYYY-MM-DD HH24:MI:SS.FF')
+		 */
+		if (driver == Driver.ORACLE) {
+			bf.insert(0, "to_date(");
+			bf.append(",'YYYY-MM-DD HH24:MI:SS')");
+		}
 		return bf.toString();
 	}
 
