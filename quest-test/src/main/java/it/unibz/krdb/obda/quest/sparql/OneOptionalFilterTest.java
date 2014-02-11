99df3521269cb286ff00374771e223d125c1f937
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2014-02-11 17:59:01 +0100

    Adding test cases for Optional SPARQL. It works correctly with one optional, but fails when nested optionals are involved

---
+++ quest-test/src/main/java/it/unibz/krdb/obda/quest/sparql/OneOptionalFilterTest.java
@@ -0,0 +1,214 @@
+package it.unibz.krdb.obda.quest.sparql;
+
+/*
+ * #%L
+ * ontop-quest-owlapi3
+ * %%
+ * Copyright (C) 2009 - 2013 Free University of Bozen-Bolzano
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.model.OBDADataFactory;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDADataFactoryImpl;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWL;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLConnection;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLFactory;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLResultSet;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLStatement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/***
+ * Check how the optional filter is converted in left join
+ * 
+ * We are going to create an H2 DB, the .sql file is fixed. We will map directly
+ * there and then query on top.
+ */
+public class OneOptionalFilterTest extends TestCase {
+
+	// TODO We need to extend this test to import the contents of the mappings
+	// into OWL and repeat everything taking form OWL
+
+	private OBDADataFactory fac;
+	private Connection conn;
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private OBDAModel obdaModel;
+	private OWLOntology ontology;
+
+	final String owlfile = "resources/optional/optional-mapping.owl";
+//	final String owlfile = "resources/optional/optional-modified.owl";
+	final String obdafile = "resources/optional/optional-mapping.obda";
+
+	@Override
+	public void setUp() throws Exception {
+		
+		
+		/*
+		 * Initializing and H2 database with the stock exchange data
+		 */
+		// String driver = "org.h2.Driver";
+		String url = "jdbc:h2:mem:questjunitdb";
+		String username = "fish";
+		String password = "fish";
+
+		fac = OBDADataFactoryImpl.getInstance();
+
+		conn = DriverManager.getConnection(url, username, password);
+		
+		Statement st = conn.createStatement();
+
+		//with simple h2 test we enter in a second nested left join and it fails
+		FileReader reader = new FileReader("resources/optional/create-simpleH2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line);
+			line = in.readLine();
+		}
+
+		st.executeUpdate(bf.toString());
+		conn.commit();
+
+		// Loading the OWL file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
+
+		// Loading the OBDA data
+		obdaModel = fac.getOBDAModel();
+		
+		ModelIOManager ioManager = new ModelIOManager(obdaModel);
+		ioManager.load(obdafile);
+		
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			dropTables();
+			conn.close();
+		} catch (Exception e) {
+			log.debug(e.getMessage());
+		}
+	}
+
+	private void dropTables() throws SQLException, IOException {
+
+		Statement st = conn.createStatement();
+
+		FileReader reader = new FileReader("resources/optional/drop-H2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line);
+			line = in.readLine();
+		}
+
+		st.executeUpdate(bf.toString());
+		st.close();
+		conn.commit();
+	}
+
+	private void runTests(Properties p) throws Exception {
+
+		// Creating a new instance of the reasoner
+		QuestOWLFactory factory = new QuestOWLFactory();
+		factory.setOBDAController(obdaModel);
+
+		factory.setPreferenceHolder(p);
+
+		QuestOWL reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
+
+		// Now we are ready for querying
+		QuestOWLConnection conn = reasoner.getConnection();
+		QuestOWLStatement st = conn.createStatement();
+
+		String query = "PREFIX : <http://it.unibz.krdb/obda/test/simple#> SELECT * WHERE{ :ID1 :REFERP ?v . OPTIONAL {  :ID3 :REFERQ ?w .   }}";
+		StringBuilder bf = new StringBuilder(query);
+		try {
+			
+			System.out.println(query);
+			QuestOWLResultSet rs = st.executeTuple(query);
+			assertTrue(rs.nextRow());
+			OWLLiteral ind1 = rs.getOWLLiteral("v");
+			OWLLiteral ind2 = rs.getOWLLiteral("w");
+			
+			assertEquals("\"1\"^^xsd:integer", ind1.toString());
+			assertEquals("\"3\"^^xsd:integer", ind2.toString());
+			
+			
+			
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+
+			} catch (Exception e) {
+				st.close();
+			}
+			conn.close();
+			reasoner.dispose();
+		}
+	}
+
+	public void testViEqSig() throws Exception {
+
+		QuestPreferences p = new QuestPreferences();
+		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+
+		runTests(p);
+	}
+	
+	public void testClassicEqSig() throws Exception {
+
+		QuestPreferences p = new QuestPreferences();
+		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.CLASSIC);
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+		p.setCurrentValueOf(QuestPreferences.OBTAIN_FROM_MAPPINGS, "true");
+
+		runTests(p);
+	}
+
+
+}
